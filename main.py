3a9238bdfa7b4072f2da536319de54c6ea806981
danieljponce <danieljpumaponce@gmail.com>
2025-04-27 21:52:09 -0500

    iniciando carrito de compras

---
+++ main.py
@@ -0,0 +1,34 @@
+WAREHOUSE = [
+    {
+        "code":"A001": ,
+        "Producto": "Pan",
+        "Precio": 1.50
+    },
+    {   "code": "B203"
+        "Producto": "Leche",
+        "Precio": 3.80
+    }
+] 
+
+
+def show_menu():
+    print("""
+    1. Ver catálogo 
+    2. Agregar producto al carrito 
+    3. Eliminar producto del carrito 
+    4. vaciar carrito 
+    5. Mostrar carrito 
+    6. Finalizar compra 
+    7. salir """)
+
+def show_catalog() :
+
+    for Producto in HAREHOUSE: 
+    pr4nMf"COdigo: (code) I Producto: (name} Precio: 
+
+
+show_menu( ) 
+
+
+
+    
